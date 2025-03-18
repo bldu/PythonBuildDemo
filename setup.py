6d1e2de09b67fc4ad642a1ca225931b4e7e2c626
Bolun Du <Bolun.Du@amlogic.com>
2025-03-18 15:14:43 +0800

    initialized

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+def get_long_description():
+    with open("README.md", "r") as f:
+        long_desc = f.read()
+    return long_desc      
+
+setuptools.setup(
+    name="my_pkg",
+    version="0.0.1",
+    author="bldu",
+    description="python build demo",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="",
+    install_requires=["opencv-python"],
+    packages=setuptools.find_packages(exclude=("tests"))
+)
